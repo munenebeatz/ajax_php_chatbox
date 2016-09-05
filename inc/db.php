56ddda18fe6275c53df05a1d7150197da72fe136
Munenebeatz <md@cordelia.co.ke>
2016-09-05 16:03:38 +0300

    initialization of files req

---
+++ inc/db.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: CORDELIA
+ * Date: 05-Sep-16
+ * Time: 4:01 PM
+ */
--- index.php
+++ index.php
@@ -1,7 +1,12 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: CORDELIA
- * Date: 05-Sep-16
- * Time: 3:47 PM
- */
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Php Shoutbox</title>
+	<link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+
+<script src="../dist/jQ/jquery-3.1.0.min.js"></script>
+<script src="script.js"></script>
+</body>
+</html>
